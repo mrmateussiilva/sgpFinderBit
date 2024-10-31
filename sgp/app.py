3237574dbde515ca86bfe3f5f5f625c4f7517829
mrmateussiilva <mateuspython3010@gmail.com>
2024-10-30 23:09:43 -0300

    Removendo  o banco de dados da homepage

--- sgp/app.py
+++ sgp/app.py
@@ -1,24 +1,18 @@
 from flask import Flask,render_template,request,redirect
 from models_dev import model
-from pedidos import core
-
-
-
-
-
-
+from pedidos import pedido
 
 
 app = Flask(__name__)
-app.register_blueprint(core.bp_pedido)
+app.register_blueprint(pedido.bp_pedido)
+
 
 @app.get("/")
 def index():
-    pedidos = model.get_all_pedidos()
     return render_template(
         "index.html"
         ,title_page="Pagina Inicial"
-        ,pedidos=pedidos.items())
+        )
 
 
 if __name__ == "__main__":
